54c4e64c681b5cd31f2fed1238f641c27c8a84f3
Kendrick Kesley <kkesley@Kendricks-MacBook-Pro.local>
2020-03-22 20:25:07 +1100

    Remove unused screen type
    Re-add when needed

--- src/themes/screen.ts
+++ src/themes/screen.ts
@@ -3,8 +3,5 @@ export default {
   phone: { maxWidth: 768 },
   tablet: { minWidth: 769, maxWidth: 959 },
   desktop: { minWidth: 960, maxWidth: 1280 },
-  hd: { minWidth: 1281, maxWidth: 1440 },
-  fhd: { minWidth: 1441, maxWidth: 1920 },
-  qhd: { minWidth: 1921, maxWidth: 2560 },
   uhd: { minWidth: 2561 },
 }
