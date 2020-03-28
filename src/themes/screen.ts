6617cc5e0b25d40a1df9c6e57c7ce1712e9c1103
Kendrick Kesley <kendrick.kesley.dev@gmail.com>
2020-03-28 23:51:26 +1100

    Change uhd mindwidth to 2500

--- src/themes/screen.ts
+++ src/themes/screen.ts
@@ -3,5 +3,5 @@ export default {
   phone: { maxWidth: 768 },
   tablet: { minWidth: 769, maxWidth: 959 },
   desktop: { minWidth: 960, maxWidth: 1280 },
-  uhd: { minWidth: 2561 },
+  uhd: { minWidth: 2500 },
 }
