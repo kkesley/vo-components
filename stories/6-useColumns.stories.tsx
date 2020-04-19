04f7b8bcd8d8e3426db928d1776f504439e3e44d
Kendrick Kesley <kkesley@Kendricks-MacBook-Pro.local>
2020-04-19 15:27:52 +1000

    Add useColumns to story

---
+++ stories/6-useColumns.stories.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { useColumns, calculateColumnOriginalIndex } from '../src'
+
+export default {
+  title: 'useColumns',
+}
+
+const items = new Array(1000).fill(null).map((_, index) => `item ${index}`)
+
+export const Default = () => {
+  const { chunkedItems, columnCount } = useColumns({ items })
+  return (
+    <div className="container">
+      <p>Registered column: {columnCount}</p>
+      <div className="columns is-multiline">
+      {chunkedItems.map((chunk, chunkIndex) => (
+         <div className="column is-4 is-6-tablet">
+          {chunk.map((item, itemIndex) => (
+           
+              <div className="card">
+                  <div className="card-content"><p>{item}</p>
+              <p>
+                Original Index:&nbsp;
+                {calculateColumnOriginalIndex(
+                  chunkIndex,
+                  itemIndex,
+                  columnCount
+                )}
+              </p></div>
+              </div>
+            
+          ))}
+        </div>
+      ))}
+      </div>
+    </div>
+  )
+}
+
+export const ColumnOverride = () => {
+    const { chunkedItems, columnCount } = useColumns({ items, columnOverride: {desktop: 6, tablet: 4, phone: 2 } })
+    return (
+      <div className="container">
+        <p>Registered column: {columnCount}</p>
+        <div className="columns is-multiline is-mobile">
+        {chunkedItems.map((chunk, chunkIndex) => (
+           <div className="column is-6-mobile is-3-tablet is-2-desktop">
+            {chunk.map((item, itemIndex) => (
+             
+                <div className="card">
+                    <div className="card-content"><p>{item}</p>
+                <p>
+                  Original Index:&nbsp;
+                  {calculateColumnOriginalIndex(
+                    chunkIndex,
+                    itemIndex,
+                    columnCount
+                  )}
+                </p></div>
+                </div>
+              
+            ))}
+          </div>
+        ))}
+        </div>
+      </div>
+    )
+  }
