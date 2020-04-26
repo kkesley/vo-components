25fe78f9307c35adf3508663d22bb7148a229573
Kendrick Kesley <kkesley@Kendricks-MacBook-Pro.local>
2020-04-26 13:15:11 +1000

    dont filter is_ready and add file_name in FlatCustomerFile

--- src/helpers/customerFiles.ts
+++ src/helpers/customerFiles.ts
@@ -9,6 +9,7 @@ import {
 export interface FlatCustomerFile {
   bucket?: string
   file_path: string
+  file_name: string | undefined
   file_id: string
   request_type: CustomerFileRequestType
   request_timestamp: string
@@ -25,25 +26,24 @@ export function flattenCustomerFiles(
   customerFiles: CustomerFile[]
 ): FlatCustomerFile[] {
   return (
-    customerFiles
-      .filter((customerFile) => customerFile.is_ready)
-      .flatMap<FlatCustomerFile>(
-        (customerFile) =>
-          customerFile.output_result?.target_paths?.map((path) => ({
-            bucket: customerFile.output_result?.target_bucket || undefined,
-            file_path: path,
-            file_id: customerFile.file_id,
-            request_type: customerFile.request_type,
-            request_timestamp: customerFile.request_timestamp,
-            requester: customerFile.requester || undefined,
-            customer_id: customerFile.customer_id,
-            collection_id: customerFile.collection_id || undefined,
-            collection_segment: customerFile.collection_segment || undefined,
-            is_ready: customerFile.is_ready || undefined,
-            type: customerFile.type || undefined,
-            system_identifier: customerFile.system_identifier || undefined,
-          })) || []
-      ) || []
+    customerFiles.flatMap<FlatCustomerFile>(
+      (customerFile) =>
+        customerFile.output_result?.target_paths?.map((path) => ({
+          bucket: customerFile.output_result?.target_bucket || undefined,
+          file_path: path,
+          file_name: path.split('/').pop(),
+          file_id: customerFile.file_id,
+          request_type: customerFile.request_type,
+          request_timestamp: customerFile.request_timestamp,
+          requester: customerFile.requester || undefined,
+          customer_id: customerFile.customer_id,
+          collection_id: customerFile.collection_id || undefined,
+          collection_segment: customerFile.collection_segment || undefined,
+          is_ready: customerFile.is_ready || undefined,
+          type: customerFile.type || undefined,
+          system_identifier: customerFile.system_identifier || undefined,
+        })) || []
+    ) || []
   )
 }
 
